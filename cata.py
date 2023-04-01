ba6856f0e61188745ed7e26686ba820517b7a715
Evernow <44484725+Evernow@users.noreply.github.com>
2023-03-31 21:52:08 -0400

    Add sorting by value

--- cata.py
+++ cata.py
@@ -1,43 +1,75 @@
-from contextlib import nullcontext
 import json
-with open('CurrentData.json') as json_file:
-    source = json.load(json_file)
-gil10kj= open("gil10k.json", "w+")
-gil100kj= open("gil100k.json", "w+")
-gil1mj=open("gil1m.json","w+")
-gil10mj=open("gil10m.json","w+")
-gilBeyondj=open("gilBeyond.json","w+")
-
+from urllib.request import urlopen, Request
 #declares the lists containing the dicts 
-gil10k, gil100k, gil1m, gil10m, gilBeyond=[],[],[],[],[]
-    
+gil10k, gil100k, gil1m, gil10m, gilBeyond={},{},{},{},{}
+
+
 #places each item into a respective price bracket
 def catagories():
-    #goes thru all items
-    for inf in source:
-     price= source[inf]["LowestPrice"]
-     #sorts out null variables
-     if price == None:
-        continue #does nothing
+   with open('CurrentData.json') as json_file:
+      source = json.load(json_file)
+   gil10kj= open("gil10k.json", "w+")
+   gil100kj= open("gil100k.json", "w+")
+   gil1mj=open("gil1m.json","w+")
+   gil10mj=open("gil10m.json","w+")
+   gilBeyondj=open("gilBeyond.json","w+")
+
+   #goes thru all items
+   for inf in source:
+      price= source[inf]["LowestPrice"]
+      #sorts out null variables
+      if price == None:
+         continue #does nothing
       #sorts thru everything and puts them into their own respective jsons based on price
       #note that this brings them into a list not a dict
-     elif price <=1000:
-         gil10k.append({inf:source[inf]})
-     elif price <=10000:
-         gil100k.append({inf:source[inf]})
-     elif price <=100000:
-         gil1m.append({inf:source[inf]})
-     elif price <=1000000:
-         gil10m.append({inf:source[inf]})
-     elif price >1000000:
-         gilBeyond.append({inf:source[inf]})
+      elif price <=1000:
+         gil10k[inf] = source[inf]
+      elif price <=10000:
+         gil100k[inf] = source[inf]
+      elif price <=100000:
+         gil1m[inf] = source[inf]
+      elif price <=1000000:
+         gil10m[inf] = source[inf]
+      elif price >1000000:
+         gilBeyond[inf] = source[inf]
  #puts everything into the jsons
-    json.dump(gil10k,gil10kj, indent = 6)
-    json.dump(gil100k, gil100kj, indent = 6)
-    json.dump(gil1m,gil1mj, indent = 6)
-    json.dump(gil10m,gil10mj,indent =6 )
-    json.dump(gilBeyond,gilBeyondj,indent =6 )
-catagories()    
+   json.dump(gil10k,gil10kj, indent = 6)
+   json.dump(gil100k, gil100kj, indent = 6)
+   json.dump(gil1m,gil1mj, indent = 6)
+   json.dump(gil10m,gil10mj,indent =6 )
+   json.dump(gilBeyond,gilBeyondj,indent =6 )
+
+def GetLowestMarketTax():
+   # Gets current lowest tax rate in Siren
+   url = f"https://universalis.app/api/v2/tax-rates?world=Siren"
+   headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.3'}
+   req = Request(url=url, headers=headers) 
+   html = urlopen(req).read() 
+   data_json = json.loads(html)
+   return min(list(data_json.values()))*0.01
+
+def SortItemsByValue():
+   MarketTax = GetLowestMarketTax()
+   sorted_dict = {}
+   buckets = ["gil10k.json","gil100k.json","gil1m.json","gil10m.json","gilBeyond.json"]
+   for bucket in buckets:
+      sorted_dict[bucket] = []
+      with open(bucket, 'r') as j:
+         contents = json.loads(j.read())
+      for i in contents:
+         index = i 
+         i = contents[i]
+         try:
+            #  ((AveragePriceSiren - (AveragePriceSiren * MarketTax )) - LowestPrice) * SalesInLastMonth * AverageQuantitySiren
+            formula =  ((i['AveragePriceSiren'] - (i['AveragePriceSiren'] * MarketTax )) - (i['LowestPrice']) * i['SalesInLastMonth'] * i['AverageQuantitySiren'])
+            sorted_dict[bucket].append((formula,index))
+         except:
+            pass
+      sorted_dict[bucket] = sorted(sorted_dict[bucket], key=lambda x: x[0], reverse=True)
+   json.dump(sorted_dict,open("ValuesSorted.json", "w+"),indent =6 )
+catagories()  
+SortItemsByValue()
+  
                
     
     
