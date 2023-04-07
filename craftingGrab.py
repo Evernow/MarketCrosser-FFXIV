8751df9c03759a83bed2d5e56c4b9631bbae4c4e
Evernow <44484725+Evernow@users.noreply.github.com>
2023-04-07 04:44:20 -0400

    get list of items on page

--- craftingGrab.py
+++ craftingGrab.py
@@ -8,6 +8,7 @@ import bs4
 import multiprocessing
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.options import Options
 import json
 import time
 class craftingGrabber:
@@ -28,13 +29,20 @@ class craftingGrabber:
     #the reason for using a method is 
     #in the hopes of maintaining expandability in case of changes to the official website
     def ffxivScraper(url,id,xpath,className,blockedterms,blockedurls):
-        driver = webdriver.Firefox()
+        options = Options()
+        options.add_argument('--headless')
+        driver = webdriver.Firefox(options=options)
         driver.get(url)
-      #  try:
-        DB = driver.find_elements(By.CLASS_NAME, className)
-        driver.get
-        for s in range(len(DB)):
-            DB[s].click()
+        list_of_items = []
+        # Get all items on page in a list (their URLs)
+        all_questions = driver.find_elements(By.CLASS_NAME, className)
+        for question in all_questions:
+            list_of_items.append(question.get_attribute("href"))
+        print(list_of_items)
+        # DB = driver.find_elements(By.CLASS_NAME, className)
+        # driver.get
+        # for s in range(len(DB)):
+        #     DB[s].click()
         
       #  except:
          #   continue
@@ -43,4 +51,5 @@ class craftingGrabber:
          #   except:
          #       DB= driver.find_element(By.ID, id)
     
-    ffxivScraper("https://na.finalfantasyxiv.com/lodestone/playguide/db/recipe/?page=1","db-table__txt--detail_link","db-table__txt--detail_link","db-table__txt--detail_link","","" )
+    x = ffxivScraper("https://na.finalfantasyxiv.com/lodestone/playguide/db/recipe/?page=1","db-table__txt--detail_link","db-table__txt--detail_link","db-table__txt--detail_link","","" )  
+    # print(x)
