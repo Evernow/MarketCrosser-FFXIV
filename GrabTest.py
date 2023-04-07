ed765e201a2efdcf6c73be0079a6147750630998
Jasper(Deadly) <35386705+JasperNelson@users.noreply.github.com>
2023-04-06 19:55:24 -0700

    Create GrabTest.py

---
+++ GrabTest.py
@@ -0,0 +1,9 @@
+#this is a file intended for testing and troubleshooting
+# craftingGrab.py EXCLUSIVELY!  
+#keep everything commented out unless you intend to use
+from selenium import webdriver
+import bs4
+
+db_path = "/html/body/div[3]/div[2]/div[1]/div/div[2]/div[2]/div[4]/div/table/tbody"
+
+
