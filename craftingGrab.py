7b324d91acfdd3592debdacb27dd9ee0c98086f1
Jasper(Deadly) <35386705+JasperNelson@users.noreply.github.com>
2023-04-09 21:19:45 -0700

    Fixed jank code to a more official code

--- craftingGrab.py
+++ craftingGrab.py
@@ -12,14 +12,18 @@ from selenium.webdriver.common.options import ArgOptions
 import json
 import time
 class craftingGrabber:
-    global links, crecipie, c_visited
+    def __init__(self):
+#standard vars
     #dictionary of items 
-    crecipie={}
-    #simple list of the items that have been visited
-    c_visited=[]
-    #list of the hyperlinks of all the items
-    links =[]
-    #returns the hyperlink of items given the 
+        self.crecipie={}
+        #simple list of the hyperlinks of items to visit
+        self.links =[]
+    #sanity checking variables
+        #list of the hyperlinks of all the items that have been visited successfully
+        self.s_visited=[]
+        #list of the hyperlinks of the items that were unsuccesfully visited
+        self.e_visited=[]
+     
 
     
         
@@ -33,34 +37,49 @@ class craftingGrabber:
     #they should all be strings
     #the reason for using a method is 
     #in the hopes of maintaining expandability in case of changes to the official website
-    def ffxivScraper(url,id,xpath,className,blockedterms,blockedurls):
-        nextprev,materials,crystals,level,job="",[],[],1,None 
-        
+    def ffxivItemHyperlinkScraper(self,url,className):
+        #initialconnect is used to count the while loop times it took to connect
+        initialconnect=True
         driver = webdriver.Firefox()
         driver.get(url)
-        x=True
-        while(x==True):
         
-        #  try:
-            DB = driver.find_elements(By.CLASS_NAME, className)
-            driver.get
-        #  this rotates thru and assigns the href value to all the functions(hyperlinks)
-            for s in range(len(DB)):
-                links.append(DB[s].get_attribute("href"))
-                #clicks the next one
+        #encased will redo the whole initial connection if the website is down after 10 seconds
+        while(initialconnect):
             try:
                 tonext=driver.find_element(By.CLASS_NAME, "next")
-                tonext.click()
+                initialconnect=False
             except:
-                x=False
+                print("something went wrong when connecting to the ffxiv server trying again in 10 mins. . .")
+                time.sleep(600)
+    #the next element is the object that identifies the next page of the db on the web
+        tonext=driver.find_element(By.CLASS_NAME, "next")
+    #the last page to scan and where to stop
+        last=driver.find_element(By.CLASS_NAME, "next_all").find_element(By.TAG_NAME, "a").get_attribute("href")
+    #the next page to scan
+        nextprev=tonext.find_element(By.TAG_NAME, "a").get_attribute("href")
+        print(last, "/n")
+        traverse=True
         
-      #  except:
-         #   continue
-         #   try:
-         #       DB = driver.find_element(By.XPATH, xpath)
-         #   except:
-         #       DB= driver.find_element(By.ID, id)
-         
-         
-         
-    ffxivScraper("https://na.finalfantasyxiv.com/lodestone/playguide/db/recipe/?page=205","db-table__txt--detail_link","db-table__txt--detail_link","db-table__txt--detail_link","","" )
+        #rotates through the entire library of craftable items 
+        while(traverse):
+            #Identifies the items and the next page
+            DB = driver.find_elements(By.CLASS_NAME, className)
+        #  this rotates thru and assigns the href value to all the functions(hyperlinks)
+            for s in range(len(DB)):
+                self.links.append(DB[s].get_attribute("href"))
+            #clicks the next page if there is one
+            if(nextprev!=last):
+                tonext=driver.find_element(By.CLASS_NAME, "next")
+                nextprev=tonext.find_element(By.TAG_NAME, "a").get_attribute("href")
+                tonext.click()
+                tonext=driver.find_element(By.CLASS_NAME, "next")
+            else:
+                traverse=False
+                break
+        return(self.links)
+    
+    def ffxivScraper(self,url,className):
+        self.ffxivItemHyperlinkScraper(url,className)
+        
+scraper=craftingGrabber() 
+scraper.ffxivScraper("https://na.finalfantasyxiv.com/lodestone/playguide/db/recipe/?page=205","db-table__txt--detail_link")
